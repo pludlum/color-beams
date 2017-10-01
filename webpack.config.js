519eb6133ff264f06c6ae129204a56509b859630
Peter Ludlum <t.broeck@gmail.com>
2017-10-01 15:11:00 -0700

    created skeleton for project, installed webpack, jquery, babel-core, babel-loader

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./lib/colorbeams.js",
+  output: {
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["*", ".js", ".jsx" ]
+  }
+};
