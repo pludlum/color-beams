a81524b4850c1bf34042e49b33d7f4ce3c00ec5c
Peter Ludlum <t.broeck@gmail.com>
2017-10-03 09:29:08 -0700

    generate beams randomly, added basic hit detection

--- bundle.js
+++ bundle.js
@@ -70,11 +70,14 @@
 "use strict";
 
 
-var _screen = __webpack_require__(2);
+var _game = __webpack_require__(1);
 
-var _screen2 = _interopRequireDefault(_screen);
+var _game2 = _interopRequireDefault(_game);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// import Screen from "./screen";
+
 
 document.addEventListener("DOMContentLoaded", function () {
 
@@ -101,18 +104,18 @@ document.addEventListener("DOMContentLoaded", function () {
       options.pathHistory.shift();
     }
   };
-
   canvasEl.addEventListener("mousemove", setMousePosition, false);
 
-  var gameScreen = new _screen2.default(canvasEl, ctx, options);
+  var game = new _game2.default(canvasEl, ctx, options);
+  // const gameScreen = new Screen(canvasEl, ctx, game, options);
+
   var draw = setInterval(function (e) {
-    gameScreen.draw(canvasEl, ctx, options);
+    game.drawAll(canvasEl, ctx, options);
   }, 10);
-}); // import Game from "./game";
+});
 
 /***/ }),
-/* 1 */,
-/* 2 */
+/* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -128,37 +131,126 @@ var _circles = __webpack_require__(3);
 
 var _circles2 = _interopRequireDefault(_circles);
 
+var _beams = __webpack_require__(4);
+
+var _beams2 = _interopRequireDefault(_beams);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Screen = function () {
-  function Screen(canvas, ctx, options) {
-    _classCallCheck(this, Screen);
+var Game = function () {
+  function Game(canvas, ctx, options) {
+    _classCallCheck(this, Game);
 
     this.ctx = ctx;
-    this.leadCircle = new _circles2.default({ "color": '#A8355C' });
-    this.followCircle = new _circles2.default({ "color": '#2A5673' });
+    this.canvas = canvas;
+    this.leadCircle = new _circles2.default({ "color": '#bf4422' });
+    this.followCircle = new _circles2.default({ "color": '#229dbf' });
+
+    this.beams = [];
+    this.circles = [this.leadCircle, this.followCircle];
+
+    this.timer = 0;
+    this.score = 0;
+
+    this.colors = ['#bf4422', '#229dbf'];
   }
 
-  _createClass(Screen, [{
-    key: 'draw',
-    value: function draw(canvasEl, ctx, options) {
+  _createClass(Game, [{
+    key: 'getRandomColor',
+    value: function getRandomColor() {
+      return this.colors[Math.round(Math.random())];
+    }
+  }, {
+    key: 'makeLeftVerticalBeam',
+    value: function makeLeftVerticalBeam() {
+      this.beams.push(new _beams2.default({ "pos": [-10, 0], "vel": [0.5 * (this.timer + 1), 0], 'height': this.canvas.height, 'width': 8, "color": this.getRandomColor() }));
+    }
+  }, {
+    key: 'makeRightVerticalBeam',
+    value: function makeRightVerticalBeam() {
+      this.beams.push(new _beams2.default({ "pos": [this.canvas.width + 10, 0], "vel": [-0.5 * (this.timer + 1), 0], 'height': this.canvas.height, 'width': 8, "color": this.getRandomColor() }));
+    }
+  }, {
+    key: 'makeTopHorizontalBeam',
+    value: function makeTopHorizontalBeam() {
+      this.beams.push(new _beams2.default({ "pos": [-10, 0], "vel": [0, 0.5 * (this.timer + 1)], 'height': 8, 'width': this.canvas.width, "color": this.getRandomColor() }));
+    }
+  }, {
+    key: 'makeBottomHorizontalBeam',
+    value: function makeBottomHorizontalBeam() {
+      this.beams.push(new _beams2.default({ "pos": [0, this.canvas.height + 10], "vel": [0, -0.5 * (this.timer + 1)], 'height': 8, 'width': this.canvas.width, "color": this.getRandomColor() }));
+    }
+  }, {
+    key: 'createBeams',
+    value: function createBeams() {
+      if (this.beams.length < Math.floor(1 + this.timer)) {
+        var selection = Math.floor(Math.random() * 4) + 1;
+        switch (selection) {
+          case 1:
+            this.makeLeftVerticalBeam();
+          case 2:
+            this.makeRightVerticalBeam();
+          case 3:
+            this.makeTopHorizontalBeam();
+          default:
+            this.makeBottomHorizontalBeam();
+        }
+      }
+    }
+  }, {
+    key: 'detectCollision',
+    value: function detectCollision() {
+      for (var i = 0; i < this.beams.length; i++) {
+        for (var j = 0; j < this.circles.length; j++) {
+          if (this.beams[i].pos[0] > this.circles[j].pos[0] - this.circles[j].radius && this.beams[i].pos[0] < this.circles[j].pos[0] + this.circles[j].radius) {
+            this.enforceCollision(this.circles[j], this.beams[i], i);
+          }
+          if (this.beams[i].pos[1] > this.circles[j].pos[1] - this.circles[j].radius && this.beams[i].pos[1] < this.circles[j].pos[1] + this.circles[j].radius) {
+            this.enforceCollision(this.circles[j], this.beams[i], i);
+          }
+        }
+      }
+    }
+  }, {
+    key: 'enforceCollision',
+    value: function enforceCollision(circle, beam, beamIdx) {
+      if (circle.color === beam.color) {
+        this.beams.splice(beamIdx, 1);
+        this.score += 1;
+      }
+    }
+  }, {
+    key: 'drawAll',
+    value: function drawAll(canvasEl, ctx, options) {
+      var _this = this;
+
+      this.timer += 0.001;
+      this.createBeams();
+
+      this.ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+
       this.leadCircle.updatePos(options.mousePos);
       this.followCircle.followPos(options.pathHistory, options.mousePos);
+      this.leadCircle.draw(ctx);
+      this.followCircle.draw(ctx);
 
-      ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
-      this.leadCircle.draw(this.ctx);
-      this.followCircle.draw(this.ctx);
+      this.beams.forEach(function (beam) {
+        beam.move();
+        beam.draw(_this.ctx);
+      });
+      this.detectCollision();
     }
   }]);
 
-  return Screen;
+  return Game;
 }();
 
-exports.default = Screen;
+exports.default = Game;
 
 /***/ }),
+/* 2 */,
 /* 3 */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -178,7 +270,7 @@ var Circle = function () {
     _classCallCheck(this, Circle);
 
     this.radius = 30;
-    this.pos = [0, 0];
+    this.pos = [600, 400];
     this.color = options.color;
   }
 
@@ -197,7 +289,6 @@ var Circle = function () {
     key: "followPos",
     value: function followPos(pathHistory, currentPos) {
       if (pathHistory.length > 30) {
-        console.log(this.findDistance(currentPos, pathHistory[0]));
         this.pos = pathHistory[0];
       }
     }
@@ -216,6 +307,51 @@ var Circle = function () {
 }();
 
 exports.default = Circle;
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Beam = function () {
+  function Beam(options) {
+    _classCallCheck(this, Beam);
+
+    this.color = options.color;
+    this.vel = options.vel;
+    this.pos = options.pos;
+    this.height = options.height;
+    this.width = options.width;
+  }
+
+  _createClass(Beam, [{
+    key: "move",
+    value: function move() {
+      this.pos[0] += this.vel[0];
+      this.pos[1] += this.vel[1];
+    }
+  }, {
+    key: "draw",
+    value: function draw(ctx) {
+      ctx.fillStyle = this.color;
+      ctx.fillRect(this.pos[0], this.pos[1], this.width, this.height);
+    }
+  }]);
+
+  return Beam;
+}();
+
+exports.default = Beam;
 
 /***/ })
 /******/ ]);
